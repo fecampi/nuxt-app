ba6a31908118ca4a7f527c00b6888cf4e6b804a4
Felipe Campinho <felipe.campinho@mirakulo.com>
2021-05-30 14:31:18 -0300

    first

---
+++ pages/posts/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="single-post-page">
+    <section class="post">
+      <h1 class="post-title">Title of the Post</h1>
+      <div class="post-details">
+        <div class="post-detail">Las update on XXX</div>
+        <div class="post-detail">Written by NAME</div>
+      </div>
+      <p>content of the post</p>
+    </section>
+    <section class="post-feedback">
+      <p class="post-content">
+        Let me know what you think about the post, send a mail to
+        <a href="mailto:feedback@email.com">feedback@email.com</a>
+      </p>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
